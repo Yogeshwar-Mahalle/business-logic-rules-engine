793316c52ab416d7f9ef7423098b68df7b7dc902
Yogeshwar Mahalle <yogeshwar.mahalle@gmail.com>
2023-12-31 22:12:44 +0530

    Support added to log error, info and debug messages in log file.

--- src/main/java/com/ybm/BLRuleEngineApplication.java
+++ src/main/java/com/ybm/BLRuleEngineApplication.java
@@ -4,8 +4,10 @@
 
 package com.ybm;
 
+import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,9 +19,15 @@ public class BLRuleEngineApplication {
 
     public static void main(String[] args) {
         LOG.info("******************************* BLRuleEngine Loading *******************************");
-        new SpringApplicationBuilder(BLRuleEngineApplication.class)
-                .run(args);
+        SpringApplication.run(BLRuleEngineApplication.class, args);
+        //new SpringApplicationBuilder(BLRuleEngineApplication.class).run(args);
         LOG.info("******************************* BLRuleEngine Started *******************************");
+    }
+
+    @PreDestroy
+    public void preDestroyGracefulShutdown() {
+
+        LOG.info("******************************* BLRuleEngine graceful shutdown started *******************************");
     }
 }
 
